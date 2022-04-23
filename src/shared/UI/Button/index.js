279a36ab09b2ef4a01786845cea09b324bc831bf
Jacques <jacquesagbenu@gmail.com>
2022-04-23 18:23:52 -0400

    UI added

---
+++ src/shared/UI/Button/index.js
@@ -0,0 +1,15 @@
+import classes from "./Button.module.css";
+
+const Button = (props) => {
+  return (
+    <button
+      disabled={props.disabled}
+      className={[classes.Button, classes[props.btnType]].join(" ")}
+      onClick={props.clicked}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
