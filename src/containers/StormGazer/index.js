5d277bb82e67a288933bf02288bc9432e9a9b10c
Stephen <empero6@gmail.com>
2022-04-23 15:50:45 -0400

    added zipcode form

--- src/containers/StormGazer/index.js
+++ src/containers/StormGazer/index.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import classes from "./StormGazer.module.css";
+
 const StormGazer = (props) => {
-    return (
-        <p>This is the beginning!!!!</p>
-    );
-}
+  const [zipCode, setZipCode] = useState("");
+
+  const handleVal = (event) => {
+    setZipCode(event.target.value);
+    console.log(zipCode);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(zipCode);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes["zip-code"]}>
+      <label htmlFor="zipCode">Zip Code</label>
+      <input value={zipCode} id="zipCode" onChange={handleVal} />
+      <button>Zip Code</button>
+    </form>
+  );
+};
 
 export default StormGazer;
