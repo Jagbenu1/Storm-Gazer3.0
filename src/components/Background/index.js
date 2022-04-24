c572b1e40765745c34cefe612336643a42b19d71
Jacques <jacquesagbenu@gmail.com>
2022-04-24 19:18:07 -0400

    refactored background

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -6,52 +6,52 @@ const Background = ({ children, icon, imageIndex }) => {
         let backgroundUrl = null;
 
         switch(icon) {
-            case ('clear-day'):
+            case ('clear-day' || '01d'):
                 const clearDay = require.context('../../../public/clear-day', true, /\.jpg$/);
                 keys = clearDay.keys().map(key => clearDay(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('clear-night'):
+            case ('clear-night' || '01n'):
                 const clearNight = require.context('../../../public/clear-night', true, /\.jpg$/);
                 keys = clearNight.keys().map(key => clearNight(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('cloudy'):
+            case ('cloudy' || '03d'):
                 const cloudy = require.context('../../../public/cloudy', true, /\.jpg$/);
                 keys = cloudy.keys().map(key => cloudy(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('fog'):
+            case ('fog' || '50d'):
                 const fog = require.context('../../../public/fog', true, /\.jpg$/);
                 keys = fog.keys().map(key => fog(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('partly-cloudy-day'):
+            case ('partly-cloudy-day' || '02d'):
                 const partlyCloudyDay = require.context('../../../public/partly-cloudy-day', true, /\.jpg$/);
                 keys = partlyCloudyDay.keys().map(key => partlyCloudyDay(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('partly-cloudy-night'):
+            case ('partly-cloudy-night' || '02n'):
                 const partlyCloudyNight = require.context('../../../public/partly-cloudy-night', true, /\.jpg$/);
                 keys = partlyCloudyNight.keys().map(key => partlyCloudyNight(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('rain'):
+            case ('rain' || '09d'):
                 const rain = require.context('../../../public/rain', true, /\.jpg$/);
                 keys = rain.keys().map(key => rain(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('sleet'):
+            case ('sleet' || '13d'):
                 const sleet = require.context('../../../public/sleet', true, /\.jpg$/);
                 keys = sleet.keys().map(key => sleet(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('snow'):
+            case ('snow' || '13d'):
                 const snow = require.context('../../../public/snow', true, /\.jpg$/);
                 keys = snow.keys().map(key => snow(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('wind'):
+            case ('wind' || '50d'):
                 const wind = require.context('../../../public/wind', true, /\.jpg$/);
                 keys = wind.keys().map(key => wind(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
@@ -66,7 +66,9 @@ const Background = ({ children, icon, imageIndex }) => {
     }
 
     return (
-        <div className={classes.Background} style={{backgroundImage: backgroundSwitch()}}></div>
+        <div className={classes.Background} style={{backgroundImage: backgroundSwitch()}}>
+            {children}
+        </div>
     );
 };
 
