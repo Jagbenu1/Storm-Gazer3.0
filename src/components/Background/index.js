2048e3433e0444fff7de0a2e54f751ff7dbbf6dd
Jacques <jacquesagbenu@gmail.com>
2022-04-24 22:58:42 -0400

    data gotten

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -6,56 +6,57 @@ const Background = ({ children, icon, imageIndex }) => {
         let backgroundUrl = null;
 
         switch(icon) {
-            case ('clear-day' || '01d'):
+            case ('01d'):
                 const clearDay = require.context('../../../public/clear-day', true, /\.jpg$/);
                 keys = clearDay.keys().map(key => clearDay(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('clear-night' || '01n'):
+            case ('01n'):
+                console.log('hello');
                 const clearNight = require.context('../../../public/clear-night', true, /\.jpg$/);
                 keys = clearNight.keys().map(key => clearNight(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('cloudy' || '03d'):
+            case ('03d'):
                 const cloudy = require.context('../../../public/cloudy', true, /\.jpg$/);
                 keys = cloudy.keys().map(key => cloudy(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('fog' || '50d'):
+            case ('50d'):
                 const fog = require.context('../../../public/fog', true, /\.jpg$/);
                 keys = fog.keys().map(key => fog(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('partly-cloudy-day' || '02d'):
+            case ('02d'):
                 const partlyCloudyDay = require.context('../../../public/partly-cloudy-day', true, /\.jpg$/);
                 keys = partlyCloudyDay.keys().map(key => partlyCloudyDay(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('partly-cloudy-night' || '02n'):
+            case ('02n'):
                 const partlyCloudyNight = require.context('../../../public/partly-cloudy-night', true, /\.jpg$/);
                 keys = partlyCloudyNight.keys().map(key => partlyCloudyNight(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('rain' || '09d'):
+            case ('09d'):
                 const rain = require.context('../../../public/rain', true, /\.jpg$/);
                 keys = rain.keys().map(key => rain(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('sleet' || '13d'):
+            case ('13d'):
                 const sleet = require.context('../../../public/sleet', true, /\.jpg$/);
                 keys = sleet.keys().map(key => sleet(key));
                 backgroundUrl = 'url(' + keys[imageIndex] + ')';
                 break;
-            case ('snow' || '13d'):
-                const snow = require.context('../../../public/snow', true, /\.jpg$/);
-                keys = snow.keys().map(key => snow(key));
-                backgroundUrl = 'url(' + keys[imageIndex] + ')';
-                break;
-            case ('wind' || '50d'):
-                const wind = require.context('../../../public/wind', true, /\.jpg$/);
-                keys = wind.keys().map(key => wind(key));
-                backgroundUrl = 'url(' + keys[imageIndex] + ')';
-                break;
+            // case ('13d'):
+            //     const snow = require.context('../../../public/snow', true, /\.jpg$/);
+            //     keys = snow.keys().map(key => snow(key));
+            //     backgroundUrl = 'url(' + keys[imageIndex] + ')';
+            //     break;
+            // case ('50d'):
+            //     const wind = require.context('../../../public/wind', true, /\.jpg$/);
+            //     keys = wind.keys().map(key => wind(key));
+            //     backgroundUrl = 'url(' + keys[imageIndex] + ')';
+            //     break;
             default:
                 const defaultImage = require.context('../../../public/default', true, /\.jpg$/);
                 keys = defaultImage.keys().map(key => defaultImage(key));
@@ -64,6 +65,8 @@ const Background = ({ children, icon, imageIndex }) => {
         }
         return backgroundUrl;
     }
+
+    console.log(icon);
 
     return (
         <div className={classes.Background} style={{backgroundImage: backgroundSwitch()}}>
--- src/containers/StormGazer/index.js
+++ src/containers/StormGazer/index.js
@@ -22,6 +22,9 @@ const StormGazer = (props) => {
   const zipKey = keys.zip;
 
   const ApiCall = async (zipCode) => {
+    //This has now been restructured based on destructuring.
+    //both api calls are no longer nested and they are able to get certain data without
+    //getting all of the data
     console.log(zipCode);
     try {
       dispatch(weatherActions.fetchWeatherStart());
@@ -38,16 +41,10 @@ const StormGazer = (props) => {
       );
       dispatch(
         weatherActions.fetchWeatherSuccess({
-          location: 10,
-          weather: 15,
+          location: { city, state },
+          weather: data,
         })
       );
-
-      // console.log(location);
-      // console.log(weather);
-
-      console.log(data);
-      console.log(results[zipCode][0]);
     } catch (error) {
       dispatch(weatherActions.fetchWeatherFail());
       console.log(zipCode);
@@ -55,34 +52,33 @@ const StormGazer = (props) => {
     }
   };
 
+  useEffect(() => {
+    //random number for background component is set at mounting of component
+    setRandomImageIndex(Math.round(Math.random() * 4));
+  }, []);
+
+  useEffect(() => {
+    //can only check to see if state changes are in effect in useEffect.
+    //null is given anywhere else
+    if (location && weather) {
+      console.log(location.state);
+      setIcon(weather.weather[0].icon); //sets icon prop to the icon from the weather API. sent to background
+      console.log(weather);
+      console.log(weather.weather[0].icon);
+    }
+  }, [location, weather]);
+
   const getData = () => {
-    setIcon(weather.current.weather.icon);
+    console.log(location);
+    // setIcon(weather.name);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
     ApiCall(zipCode);
-    console.log(location);
-    console.log(weather);
-    setRandomImageIndex(Math.round(Math.random() * 4));
-    // getData();
-    // let icon = "",
-    //   data = "";
-
-    // if (location && weather) {
-    //   console.log(icon);
-    // } else {
-    //   console.log(weather);
-    //   console.log(location);
-    //   console.log(icon);
-    // }
+    setRandomImageIndex(Math.round(Math.random() * 4)); // set random number prop to the background component
+    getData();
   };
-
-  useEffect(() => {
-    setRandomImageIndex(Math.round(Math.random() * 4));
-    //setIcon(weather.current.weather.icon);
-  }, [weather]);
-
   return (
     <Background icon={icon} imageIndex={randomImageIndex}>
       <form
