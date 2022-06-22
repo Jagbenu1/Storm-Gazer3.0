f4c76eeaae0a5903079359d59a8a84907842ff9f
Jacques <jacquesagbenu@gmail.com>
2022-06-22 14:12:47 -0400

    keys changed

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,6 @@
 console.log(process.env.NODE_ENV);
-export const keys =
-  process.env.NODE_ENV === "production"
-    ? import("./prod").then(({ keysProd }) => keysProd)
-    : import("./.dev").then(({ keysDev }) => keysDev);
+export const keys = {
+    weather: "72e86c0d4693758aabf2ad32b7068bc4",
+    zip: "e29d5e30-c34c-11ec-8bb7-2d5b0020755f",
+  };
 
--- src/config/prod.js
+++
@@ -1,4 +0,0 @@
-export const keysProd = {
-    weather: process.env.WEATHER_KEY,
-    zip: process.env.ZIPCODE_KEY,
-  };
