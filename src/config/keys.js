0cfd65be614b198fdf7d934ed32fcc5f3dd0e4ea
Jacques <jacquesagbenu@gmail.com>
2022-06-22 11:12:15 -0400

    dev keys changed

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,4 @@
-import { keysDev } from "./.dev";
-import { keysProd } from "./prod";
-
-export const keys = (process.env.NODE_ENV === "production") ? keysProd : keysDev;
+export const keys =
+  process.env.NODE_ENV === "production"
+    ? import("./prod").then(({ keysProd }) => keysProd)
+    : import("./.dev").then(({ keysDev }) => keysDev);
